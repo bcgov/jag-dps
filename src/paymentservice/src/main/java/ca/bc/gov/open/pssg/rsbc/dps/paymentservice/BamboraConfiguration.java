2b2a0adddb5705a6d0c1b72454a2f3b349ad7e11
Nancy <nancy.zacharias@outlook.com>
2019-12-30 11:39:01 -0800

    bambora config reading

--- src/paymentservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/paymentservice/BamboraConfiguration.java
+++ src/paymentservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/paymentservice/BamboraConfiguration.java
@@ -1,5 +1,7 @@
 package ca.bc.gov.open.pssg.rsbc.dps.paymentservice;
 
+import ca.bc.gov.open.pssg.rsbc.dps.paymentservice.types.BeanstreamEndpointResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BamboraConfiguration {
 
+  @Value("${dps.beanstream.endpoint.approved}")
+  private String approved;
+  @Value("${dps.beanstream.endpoint.declined}")
+  private String declined;
+  @Value("${dps.beanstream.endpoint.error}")
+  private String error;
+
   @RequestMapping("/bamboraconfiguration")
-  public String singlepaymenturl(@RequestParam(value="name", defaultValue="defaultvalue") String name) {
-    return "Hello from the Bambora configuration controller";
+  public BeanstreamEndpointResponse singlepaymenturl(@RequestParam(value="name", defaultValue="defaultvalue") String name) {
+    System.out.println(approved);
+    return new BeanstreamEndpointResponse(approved,declined,error,PaymentServiceConstants.PAYMENT_SERVICE_RESP_MSG_OK, PaymentServiceConstants.PAYMENT_SERVICE_SUCCESS_CD) ;
+            //"Hello from the Bambora configuration controller";
   }
 }
---
+++ src/paymentservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/paymentservice/types/BeanstreamEndpointResponse.java
@@ -0,0 +1,72 @@
+package ca.bc.gov.open.pssg.rsbc.dps.paymentservice.types;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+//@Configuration
+//@ConfigurationProperties(prefix = "dps.bambora.endpoint")
+@XmlRootElement
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class BeanstreamEndpointResponse {
+
+    private String approved;
+    private String declined;
+    private String error;
+    private String respMsg;
+    private int respCode;
+
+    public BeanstreamEndpointResponse( String approved, String declined, String error, String respMsg, int respCode){
+        this.approved = approved;
+        this.declined = declined;
+        this.error =error;
+        this.respMsg = respMsg;
+        this.respCode = respCode;
+    }
+
+    public String getRespMsg() {
+        return respMsg;
+    }
+
+    public void setRespMsg(String respMsg) {
+        this.respMsg = respMsg;
+    }
+
+    public int getRespCode() {
+        return respCode;
+    }
+
+    public void setRespCode(int respCode) {
+        this.respCode = respCode;
+    }
+
+
+
+    public String getDeclined() {
+        return declined;
+    }
+
+    public void setDeclined(String declined) {
+        this.declined = declined;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+
+    public String getApproved() {
+        return approved;
+    }
+
+    public void setApproved(String approved) {
+        this.approved = approved;
+    }
+}
