7195677c95dc914f9c2564e0993d4c44ae684168
Nancy <nancy.zacharias@outlook.com>
2020-01-02 11:44:12 -0800

    DPSIM86 error handling

--- src/paymentservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/paymentservice/BamboraConfiguration.java
+++ src/paymentservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/paymentservice/BamboraConfiguration.java
@@ -1,6 +1,9 @@
 package ca.bc.gov.open.pssg.rsbc.dps.paymentservice;
 
+import ca.bc.gov.open.pssg.rsbc.dps.paymentservice.exception.PaymentServiceException;
 import ca.bc.gov.open.pssg.rsbc.dps.paymentservice.types.BeanstreamEndpointResponse;
+import ca.bc.gov.open.pssg.rsbc.dps.paymentservice.types.SinglePaymentResponse;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,8 +21,19 @@ public class BamboraConfiguration {
 
 	@RequestMapping(value = "/bamboraconfiguration", method = RequestMethod.GET)
 	public BeanstreamEndpointResponse singlepaymenturl() {
-		return new BeanstreamEndpointResponse(approved, declined, error,
-				PaymentServiceConstants.PAYMENT_SERVICE_RESP_MSG_OK,
-				PaymentServiceConstants.PAYMENT_SERVICE_SUCCESS_CD);
+		try {
+			// Make sure autowired params are not null (May be missing from configMap)
+			if ( StringUtils.isEmpty(approved) || StringUtils.isEmpty(declined) || StringUtils.isEmpty(error))
+				throw new PaymentServiceException(PaymentServiceConstants.PAYMENT_SERVICE_ERR_MISSING_CONFIG_PARAMS);
+			return new BeanstreamEndpointResponse(approved, declined, error,
+					PaymentServiceConstants.PAYMENT_SERVICE_RESP_MSG_OK,
+					PaymentServiceConstants.PAYMENT_SERVICE_SUCCESS_CD);
+		}
+		catch (PaymentServiceException e)
+		{
+			e.printStackTrace();
+			return new BeanstreamEndpointResponse("","","",PaymentServiceConstants.PAYMENT_SERVICE_RESP_MSG_FAIL,
+					PaymentServiceConstants.PAYMENT_SERVICE_FAILURE_CD);
+		}
 	}
 }
