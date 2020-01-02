86cc77bf0e1b86ada5681210ececdf04e538c804
Nancy <nancy.zacharias@outlook.com>
2020-01-02 12:09:51 -0800

    removed the try catch as per the review comments

--- src/paymentservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/paymentservice/BamboraConfiguration.java
+++ src/paymentservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/paymentservice/BamboraConfiguration.java
@@ -1,8 +1,6 @@
 package ca.bc.gov.open.pssg.rsbc.dps.paymentservice;
 
-import ca.bc.gov.open.pssg.rsbc.dps.paymentservice.exception.PaymentServiceException;
 import ca.bc.gov.open.pssg.rsbc.dps.paymentservice.types.BeanstreamEndpointResponse;
-import ca.bc.gov.open.pssg.rsbc.dps.paymentservice.types.SinglePaymentResponse;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,28 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BamboraConfiguration {
 
-	@Value("${dps.crc.endpoint.approved}")
-	private String approved;
-	@Value("${dps.crc.endpoint.declined}")
-	private String declined;
-	@Value("${dps.crc.endpoint.error}")
-	private String error;
+    @Value("${dps.crc.endpoint.approved}")
+    private String approved;
+    @Value("${dps.crc.endpoint.declined}")
+    private String declined;
+    @Value("${dps.crc.endpoint.error}")
+    private String error;
 
-	@RequestMapping(value = "/bamboraconfiguration", method = RequestMethod.GET)
-	public BeanstreamEndpointResponse singlepaymenturl() {
-		try {
-			// Make sure autowired params are not null (May be missing from configMap)
-			if ( StringUtils.isEmpty(approved) || StringUtils.isEmpty(declined) || StringUtils.isEmpty(error))
-				throw new PaymentServiceException(PaymentServiceConstants.PAYMENT_SERVICE_ERR_MISSING_CONFIG_PARAMS);
-			return new BeanstreamEndpointResponse(approved, declined, error,
-					PaymentServiceConstants.PAYMENT_SERVICE_RESP_MSG_OK,
-					PaymentServiceConstants.PAYMENT_SERVICE_SUCCESS_CD);
-		}
-		catch (PaymentServiceException e)
-		{
-			e.printStackTrace();
-			return new BeanstreamEndpointResponse("","","",PaymentServiceConstants.PAYMENT_SERVICE_RESP_MSG_FAIL,
+    @RequestMapping(value = "/bamboraconfiguration", method = RequestMethod.GET)
+    public BeanstreamEndpointResponse singlepaymenturl() {
+		// TODO - Complete logging once available.
+        // Make sure autowired params are not null (May be missing from configMap)
+        if (StringUtils.isEmpty(approved) || StringUtils.isEmpty(declined) || StringUtils.isEmpty(error)) {
+			return new BeanstreamEndpointResponse("", "", "", PaymentServiceConstants.PAYMENT_SERVICE_RESP_MSG_FAIL,
 					PaymentServiceConstants.PAYMENT_SERVICE_FAILURE_CD);
 		}
-	}
+        else
+            return new BeanstreamEndpointResponse(approved, declined, error,
+                    PaymentServiceConstants.PAYMENT_SERVICE_RESP_MSG_OK,
+                    PaymentServiceConstants.PAYMENT_SERVICE_SUCCESS_CD);
+
+    }
 }
