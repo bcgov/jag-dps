e2d06ad44c8b32a5881ede9cf14079dba096192b
Nancy <nancy.zacharias@outlook.com>
2020-01-10 09:22:55 -0800

    code formatted

--- src/dpsvalidationservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/dpsvalidationservice/DpsValidationserviceApplication.java
+++ src/dpsvalidationservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/dpsvalidationservice/DpsValidationserviceApplication.java
@@ -4,16 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 
  * @author smillar
- *
  */
 @SpringBootApplication
 public class DpsValidationserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DpsValidationserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DpsValidationserviceApplication.class, args);
+    }
 
 }
 
--- src/dpsvalidationservice/src/test/java/ca/bc/gov/open/pssg/rsbc/dps/dpsvalidationservice/DpsValidationserviceApplicationTests.java
+++ src/dpsvalidationservice/src/test/java/ca/bc/gov/open/pssg/rsbc/dps/dpsvalidationservice/DpsValidationserviceApplicationTests.java
@@ -1,15 +1,7 @@
 package ca.bc.gov.open.pssg.rsbc.dps.dpsvalidationservice;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -19,6 +11,6 @@ class DpsValidationserviceApplicationTests {
      * contextLoaded - Test for bean context first. If no beans, other tests will fail.
      */
 
-   }
+}
 
 
