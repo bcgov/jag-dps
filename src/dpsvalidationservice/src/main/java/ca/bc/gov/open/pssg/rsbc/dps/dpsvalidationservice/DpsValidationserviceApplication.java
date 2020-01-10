ae6d5a453b39c531def1d0cd1473bb682f553c0e
Nancy <nancy.zacharias@outlook.com>
2020-01-10 09:43:12 -0800

    review comments fixed

--- src/dpsvalidationservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/dpsvalidationservice/DpsValidationserviceApplication.java
+++ src/dpsvalidationservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/dpsvalidationservice/DpsValidationserviceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author smillar
+ * @author nancymz
  */
 @SpringBootApplication
 public class DpsValidationserviceApplication {
--- src/dpsvalidationservice/src/test/java/ca/bc/gov/open/pssg/rsbc/dps/dpsvalidationservice/DpsValidationserviceApplicationTests.java
+++ src/dpsvalidationservice/src/test/java/ca/bc/gov/open/pssg/rsbc/dps/dpsvalidationservice/DpsValidationserviceApplicationTests.java
@@ -3,13 +3,13 @@ package ca.bc.gov.open.pssg.rsbc.dps.dpsvalidationservice;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-
+/**
+ * @author nancymz
+ */
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class DpsValidationserviceApplicationTests {
 
-    /**
-     * contextLoaded - Test for bean context first. If no beans, other tests will fail.
-     */
+   /* TO DO - Add test cases */
 
 }
 
