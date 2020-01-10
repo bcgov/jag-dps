65bd7f193dae654db7dc7e8cadb09e86ab4a6582
Shaun Millar <shaun.millar@nttdata.com>
2020-01-10 14:08:57 -0800

    DPSIM-99: First commit, creation of basic Springboot framework.

---
+++ src/figarovalidationservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/figvalidationservice/FigValidationServiceApp.java
@@ -0,0 +1,19 @@
+package ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author smillar
+ *
+ */
+@SpringBootApplication
+public class FigValidationServiceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FigValidationServiceApp.class, args);
+	}
+
+}
+
---
+++ src/figarovalidationservice/src/test/test/java/ca/bc/gov/pssg/rsbc/dps/figvalidationservice/FigValidationServiceAppTests.java
@@ -0,0 +1,19 @@
+package ca.bc.gov.pssg.rsbc.dps.figvalidationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 
+ * @author smillar
+ *
+ */
+@SpringBootTest
+class PaymentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
