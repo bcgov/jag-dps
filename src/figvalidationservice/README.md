a88f3bc532da6c7753782a43032981037c58403a
Shaun Millar <shaun.millar@nttdata.com>
2020-01-14 14:01:44 -0800

    DPSIM-99_3: Added swagger support and updated project level README.

--- src/figvalidationservice/README.md
+++ src/figvalidationservice/README.md
@@ -1,1 +1,89 @@
-placeholder file. 
+# FIGARO Validation Service 
+
+The FIGARO Validation Service provides **seven** operations completely replacing the legacy FIGARO Validation services previosuly hosted on webMethods. 
+
+These include: 
+
+- **locateMatchingApplicants**
+- **validateApplicantForSharing**
+- **validateApplicantPartyId**
+- **validateApplicantService**
+- **validateFacilityParty**
+- **validateOrgDrawDownBalance**
+- **validateOrgParty**
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. 
+
+See **Deployment** section for additional notes on how to deploy the project on a live system.
+
+### Configuration
+
+Application configuration information here
+ 
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+Say what the step will be
+
+```
+Give the example
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Running the tests
+
+Explain how to run the automated tests for this system
+
+### Break down into end to end tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+### And coding style tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system
+
+## Built With
+
+TBD
+
+## Contributing
+
+TBD
+
+## Versioning
+
+TBD
+
+## Authors
+
+TBD
+
+## Acknowledgments
+
+TBD
+
+
+
+
--- src/figvalidationservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/figvalidationservice/LocateMatchingApplicantsController.java
+++ src/figvalidationservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/figvalidationservice/LocateMatchingApplicantsController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.exception.FigaroValidationServiceException;
 import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.types.LocateMatchingApplicantsRequest;
 import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.types.LocateMatchingApplicantsResponse;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 /**
  * 
@@ -24,27 +28,28 @@ public class LocateMatchingApplicantsController {
 	private FigaroValidationImpl figservice;  // connection to ORDS client. 
 	
 	@RequestMapping(value = "/locateMatchingApplicants",
-	  produces = { "application/xml" }, 
-	  method = RequestMethod.GET)
+			produces = { "application/xml" }, 
+			method = RequestMethod.GET)
+		@ApiOperation(value = "Locate Matching Applicant Search", notes = "", response = LocateMatchingApplicantsResponse.class, tags={ "Figaro Validation Services"})
+	  	@ApiResponses(value = {@ApiResponse(code = 200, message = "Successful operation", response = LocateMatchingApplicantsResponse.class) })
 	public LocateMatchingApplicantsResponse locateMatchingApplicants(
-			@RequestParam(value="applSurname", defaultValue="") String applSurname,
-			@RequestParam(value="applFirstName", defaultValue="") String applFirstName,
-			@RequestParam(value="applSecondInitial", defaultValue="") String applSecondInitial,
-			@RequestParam(value="applBirthDate", defaultValue="") String applBirthDate,
-			@RequestParam(value="applDriversLicence", defaultValue="") String applDriversLicence,
-			@RequestParam(value="applBirthPlace", defaultValue="") String applBirthPlace,
-			@RequestParam(value="applGenderTxt", defaultValue="") String applGenderTxt,
-			@RequestParam(value="applAliasSurname1", defaultValue="") String applAliasSurname1,
-			@RequestParam(value="applAliasFirstName1", defaultValue="") String applAliasFirstName1,
-			@RequestParam(value="applAliasSecondInitial1", defaultValue="") String applAliasSecondInitial1,
-			@RequestParam(value="applAliasSurname2", defaultValue="") String applAliasSurname2,
-			@RequestParam(value="applAliasFirstName2", defaultValue="") String applAliasFirstName2,
-			@RequestParam(value="applAliasSecondInitial2", defaultValue="") String applAliasSecondInitial2,
-			@RequestParam(value="applAliasSurname3", defaultValue="") String applAliasSurname3,
-			@RequestParam(value="applAliasFirstName3", defaultValue="") String applAliasFirstName3,
-			@RequestParam(value="applAliasSecondInitial3", defaultValue="") String applAliasSecondInitial3) throws FigaroValidationServiceException {
+			@ApiParam(value = "applSurname", required = false) @RequestParam(value="applSurname", defaultValue="") String applSurname,
+			@ApiParam(value = "applFirstName", required = false) @RequestParam(value="applFirstName", defaultValue="") String applFirstName,
+			@ApiParam(value = "applSecondInitial", required = false) @RequestParam(value="applSecondInitial", defaultValue="") String applSecondInitial,
+			@ApiParam(value = "applBirthDate", required = false) @RequestParam(value="applBirthDate", defaultValue="") String applBirthDate,
+			@ApiParam(value = "applDriversLicence", required = false) @RequestParam(value="applDriversLicence", defaultValue="") String applDriversLicence,
+			@ApiParam(value = "applBirthPlace", required = false) @RequestParam(value="applBirthPlace", defaultValue="") String applBirthPlace,
+			@ApiParam(value = "applGenderTxt", required = false) @RequestParam(value="applGenderTxt", defaultValue="") String applGenderTxt,
+			@ApiParam(value = "applAliasSurname1", required = false) @RequestParam(value="applAliasSurname1", defaultValue="") String applAliasSurname1,
+			@ApiParam(value = "applAliasFirstName1", required = false) @RequestParam(value="applAliasFirstName1", defaultValue="") String applAliasFirstName1,
+			@ApiParam(value = "applAliasSecondInitial1", required = false) @RequestParam(value="applAliasSecondInitial1", defaultValue="") String applAliasSecondInitial1,
+			@ApiParam(value = "applAliasSurname2", required = false) @RequestParam(value="applAliasSurname2", defaultValue="") String applAliasSurname2,
+			@ApiParam(value = "applAliasFirstName2", required = false) @RequestParam(value="applAliasFirstName2", defaultValue="") String applAliasFirstName2,
+			@ApiParam(value = "applAliasSecondInitial2", required = false) @RequestParam(value="applAliasSecondInitial2", defaultValue="") String applAliasSecondInitial2,
+			@ApiParam(value = "applAliasSurname3", required = false) @RequestParam(value="applAliasSurname3", defaultValue="") String applAliasSurname3,
+			@ApiParam(value = "applAliasFirstName3", required = false) @RequestParam(value="applAliasFirstName3", defaultValue="") String applAliasFirstName3,
+			@ApiParam(value = "applAliasSecondInitial3", required = false) @RequestParam(value="applAliasSecondInitial3", defaultValue="") String applAliasSecondInitial3) throws FigaroValidationServiceException {
 		 
-		
 		 return figservice.locateMatchingApplicants(
 				
 				new LocateMatchingApplicantsRequest( 
