1db75fe78dccb05aecb10d6259ad4f4613c1cfe7
Archana <Archana.SudhakaranNair@gov.bc.ca>
2020-01-16 12:45:20 -0800

    Included Test case and Exception handling logging to ValidateApplicantForSharing

--- src/figvalidationservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/figvalidationservice/ValidateApplicantForSharingController.java
+++ src/figvalidationservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/figvalidationservice/ValidateApplicantForSharingController.java
@@ -1,5 +1,7 @@
 package ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.exception.FigaroValidationServiceException;
 import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.types.ValidateApplicantForSharingRequest;
 import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.types.ValidateApplicantForSharingResponse;
+import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.types.ValidateApplicantServiceRequest;
+import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.types.ValidateApplicantServiceResponse;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
@@ -24,21 +28,35 @@ import io.swagger.annotations.ApiResponses;
 @RestController
 public class ValidateApplicantForSharingController {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
 	private FigaroValidationImpl figservice; // connection to ORDS client.
 
-	@RequestMapping(value = "/validateApplicantForSharing", produces = {
-			"application/xml" }, method = RequestMethod.GET)
-	@ApiOperation(value = "Validate Applicant For Sharing", notes = "", response = ValidateApplicantForSharingResponse.class, tags={ "Figaro Validation Services"})
-  	@ApiResponses(value = {@ApiResponse(code = 200, message = "Successful operation", response = ValidateApplicantForSharingResponse.class) })
-    
+	@RequestMapping(value = "/validateApplicantForSharing", produces = {"application/xml" }, method = RequestMethod.GET)
+	
+	@ApiOperation(value = "Validate Applicant For Sharing", notes = "", response = ValidateApplicantForSharingResponse.class, tags = {
+			"Figaro Validation Services" })
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Successful operation", response = ValidateApplicantForSharingResponse.class) })
+
 	public ValidateApplicantForSharingResponse validateApplicantForSharingResponse(
-			@ApiParam(value = "applPartyId", required = false) @RequestParam(value = "applPartyId", defaultValue = "0",required = false) String applPartyId,
-			@ApiParam(value = "jurisdictionType", required = false) @RequestParam(value = "jurisdictionType", defaultValue = "",required = false) String jurisdictionType)
+			@ApiParam(value = "applPartyId", required = false) @RequestParam(value = "applPartyId", defaultValue = "0", required = false) String applPartyId,
+			@ApiParam(value = "jurisdictionType", required = false) @RequestParam(value = "jurisdictionType", defaultValue = "", required = false) String jurisdictionType)
 			throws FigaroValidationServiceException {
 
-		return figservice
-				.validateApplicantForSharing(new ValidateApplicantForSharingRequest(applPartyId, jurisdictionType));
+		try {
+
+			return figservice
+					.validateApplicantForSharing(new ValidateApplicantForSharingRequest(applPartyId, jurisdictionType));
+
+		} catch (FigaroValidationServiceException ex) {
+			logger.error("Exception caught as ValidateApplicantService : " + ex.getMessage());
+			ex.printStackTrace();
+			return new ValidateApplicantForSharingResponse(ex.getMessage(),
+					FigaroValidationServiceConstants.VALIDATION_SERVICE_BOOLEAN_FALSE,
+					FigaroValidationServiceConstants.VALIDATION_SERVICE_FAILURE_CD);
+		}
 	}
 
 }
