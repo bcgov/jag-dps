f107116c74970f787f58d922fa3e0527c2e1cbd6
Archana <Archana.SudhakaranNair@gov.bc.ca>
2020-01-26 21:31:26 -0800

    Changes for DPSIM-120
    Update ORDS Java Spring boot Client with new ords endpoint  for 'validateApplicantForSharing' in Figarovalidation service

--- src/figvalidationservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/figvalidationservice/FigaroValidationImpl.java
+++ src/figvalidationservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/figvalidationservice/FigaroValidationImpl.java
@@ -4,6 +4,7 @@ import ca.bc.gov.open.ords.figcr.client.api.FigvalidationsApi;
 import ca.bc.gov.open.ords.figcr.client.api.handler.ApiException;
 import ca.bc.gov.open.ords.figcr.client.api.model.ValidateApplicantForSharingOrdsResponse;
 import ca.bc.gov.open.ords.figcr.client.api.model.ValidateApplicantPartyIdOrdsResponse;
+import ca.bc.gov.open.ords.figcr.client.api.model.ValidateOrgApplicantServiceOrdsResponse;
 import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.exception.FigaroValidationServiceException;
 import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.types.*;
 import org.slf4j.Logger;
@@ -49,24 +50,24 @@ public class FigaroValidationImpl implements FigaroValidation {
         return resp;
 
     }
-
+  
     @Override
-    public ValidateApplicantServiceResponse validateApplicantService(ValidateApplicantServiceRequest lmr)
-            throws FigaroValidationServiceException {
+    /*
+     * service method to get the response for /validateOrgApplicantServiceOrdsResponse requests
+     */
+	public ValidateOrgApplicantServiceOrdsResponse validateOrgApplicantServiceOrdsResponse(String applPartyId,
+			String orgPartyId) throws FigaroValidationServiceException {
 
-        // TODO - replace the following dummy response with a call to ORDS
+		try {
+			return ordsapi.validateOrgApplicantService(applPartyId, orgPartyId, null);
+		} catch (ApiException ex) {
+			logger.error("Exception caught at Figaro Validation Service, validateOrgApplicantServiceOrdsResponse() : "
+					+ ex.getMessage());
+			ex.printStackTrace();
+			throw new FigaroValidationServiceException(ex.getMessage());
+		}
 
-        // Note: Call response code and response message comes from the ORDS call.
-
-        ValidateApplicantServiceResponse resp = new ValidateApplicantServiceResponse(
-                "success",
-                0,
-                "N"
-        );
-
-        return resp;
-
-    }
+	}
 
     @Override
     /*
--- src/figvalidationservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/figvalidationservice/ValidateApplicantServiceController.java
+++ src/figvalidationservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/figvalidationservice/ValidateApplicantServiceController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import ca.bc.gov.open.ords.figcr.client.api.model.ValidateOrgApplicantServiceOrdsResponse;
 import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.exception.FigaroValidationServiceException;
 import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.types.ValidateApplicantServiceRequest;
 import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.types.ValidateApplicantServiceResponse;
@@ -44,18 +45,19 @@ public class ValidateApplicantServiceController {
 		 
 		
 		try {
-			
-			 return figservice.validateApplicantService(
-					
-					new ValidateApplicantServiceRequest( 
-							orgPartyId,
-							applPartyId));
-		 
+
+			ValidateOrgApplicantServiceOrdsResponse _ordsResponse = figservice
+					.validateOrgApplicantServiceOrdsResponse(applPartyId, orgPartyId);
+
+			return new ValidateApplicantServiceResponse(_ordsResponse.getStatusMessage(),
+					Integer.parseInt(_ordsResponse.getStatusCode()), _ordsResponse.getValidationResult()
+
+			);
+
 		} catch (FigaroValidationServiceException ex) {
-			logger.error("Exception caught as ValidateApplicantService : " + ex.getMessage()); 
+			logger.error("Exception caught at ValidateApplicantServiceController : " + ex.getMessage());
 			ex.printStackTrace();
-			return new ValidateApplicantServiceResponse(
-					ex.getMessage(),
+			return new ValidateApplicantServiceResponse(ex.getMessage(),
 					FigaroValidationServiceConstants.VALIDATION_SERVICE_FAILURE_CD,
 					FigaroValidationServiceConstants.VALIDATION_SERVICE_BOOLEAN_FALSE);
 		}
