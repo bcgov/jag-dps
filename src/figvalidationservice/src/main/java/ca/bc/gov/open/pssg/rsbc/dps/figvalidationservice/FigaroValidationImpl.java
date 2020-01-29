37afab769a5bfad7b48f119817ef467be9568cdb
alex <alex.joyeux@gov.bc.ca>
2020-01-29 11:54:04 -0800

    added integration with ORDS for locate matching applicant

--- src/figvalidationservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/figvalidationservice/FigaroValidationImpl.java
+++ src/figvalidationservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/figvalidationservice/FigaroValidationImpl.java
@@ -7,8 +7,6 @@ import ca.bc.gov.open.ords.figcr.client.api.model.ValidateApplicantPartyIdOrdsRe
 import ca.bc.gov.open.ords.figcr.client.api.model.ValidateApplicantServiceOrdsResponse;
 import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.applicant.types.ValidateApplicantForSharingRequest;
 import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.exception.FigaroValidationServiceException;
-import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.types.LocateMatchingApplicantsRequest;
-import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.types.LocateMatchingApplicantsResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,29 +27,6 @@ public class FigaroValidationImpl implements FigaroValidation {
     @Autowired
     FigvalidationsApi ordsapi;
 
-    @Override
-    public LocateMatchingApplicantsResponse locateMatchingApplicants(LocateMatchingApplicantsRequest lmr)
-            throws FigaroValidationServiceException {
-
-        // TODO - replace the following dummy response with a call to ORDS
-
-        // Note: Call response code and response message comes from the ORDS call.
-
-        LocateMatchingApplicantsResponse resp = new LocateMatchingApplicantsResponse(
-                "Success - found Party Id 209",
-                1,
-                "209",
-                "MOUSE",
-                "MINNIE",
-                null,
-                "1950-01-10",
-                "999999",
-                "DISNEY",
-                "F");
-
-        return resp;
-
-    }
   
     @Override
     /*
--- src/figvalidationservice/src/test/java/ca/bc/gov/open/pssg/rsbc/dps/figvalidationservice/FigValidationServiceAppTests.java
+++ src/figvalidationservice/src/test/java/ca/bc/gov/open/pssg/rsbc/dps/figvalidationservice/FigValidationServiceAppTests.java
@@ -1,7 +1,5 @@
 package ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.TestPropertySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * 
@@ -34,25 +34,6 @@ public class FigValidationServiceAppTests {
 	@Test
 	void contextLoaded() {
 		assertThat(figValidationServiceApp).isNotNull();
-	}
-	
-	
-	/**
-	 * locateMatchingApplicantsHttpResponseTest - Basic HTTP test of the locateMatchingApplicants validation operation endpoint.
-	 * 
-	 * This test is NOT expected to fetch and test the response against any real data, rather we just want an HTTP response from the 
-	 * operation indicating it's working. 
-	 * 
-	 * TODO - This will require recoding once ORDS has been connected.
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	void locateMatchingApplicantsHttpResponseTest() throws Exception {
-		String request = "/locateMatchingApplicants?applSurname=abcdef99901";
-		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/figvalidationservice" + request,
-		        String.class)
-		).contains("<respCode>1</respCode>");	
 	}
 	
 	 
