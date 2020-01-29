26a82a475b15515b7757224f19aa6f4af51d6cf9
alex <alex.joyeux@gov.bc.ca>
2020-01-28 21:11:02 -0800

    Refactored ApplicantForSharing endpoint

--- src/figvalidationservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/figvalidationservice/applicant/ApplicantController.java
+++ src/figvalidationservice/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/figvalidationservice/applicant/ApplicantController.java
@@ -1,51 +1,55 @@
-package ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.controller;
+package ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.applicant;
 
 import ca.bc.gov.open.ords.figcr.client.api.model.ValidateApplicantForSharingOrdsResponse;
-import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.FigaroValidationImpl;
 import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.FigaroValidationServiceConstants;
+import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.applicant.types.ValidateApplicantForSharingRequest;
+import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.applicant.types.ValidateApplicantForSharingResponse;
 import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.exception.FigaroValidationServiceException;
-import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.types.ValidateApplicantForSharingRequest;
-import ca.bc.gov.open.pssg.rsbc.dps.figvalidationservice.types.ValidateApplicantForSharingResponse;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * ValidateApplicantForSharingController class to handle requests for /validateApplicantForSharing
+ * A Rest Controller for Applicant related operations.
  *
  * @author archanasudha
+ * @author alexjoybc@github
  */
 @RestController
-public class ValidateApplicantForSharingController {
+public class ApplicantController {
+
+    private final ApplicantService applicantService;
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Autowired
-    private FigaroValidationImpl figservice; // connection to ORDS client.
+    public ApplicantController(ApplicantService applicantService) {
+        this.applicantService = applicantService;
+    }
 
     @RequestMapping(value = "/validateApplicantForSharing", produces = {"application/xml"}, method = RequestMethod.GET)
 
-    @ApiOperation(value = "Validate Applicant For Sharing", notes = "", response = ValidateApplicantForSharingResponse.class, tags = {
-            "Figaro Validation Services"})
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successful operation", response = ValidateApplicantForSharingResponse.class)})
-
+    @ApiOperation(value = "Validate Applicant For Sharing", notes = "", response =
+            ValidateApplicantForSharingResponse.class, tags = {"Applicant"})
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successful operation", response =
+            ValidateApplicantForSharingResponse.class)})
     public ValidateApplicantForSharingResponse validateApplicantForSharing(
-            @ApiParam(value = "applPartyId", required = false) @RequestParam(value = "applPartyId", defaultValue = "0", required = false) String applPartyId,
-            @ApiParam(value = "jurisdictionType", required = false) @RequestParam(value = "jurisdictionType", defaultValue = "", required = false) String jurisdictionType)
-            throws FigaroValidationServiceException {
+            @ApiParam(value = "applPartyId", required = false) @RequestParam(value = "applPartyId", defaultValue = "0"
+                    , required = false) String applPartyId,
+            @ApiParam(value = "jurisdictionType", required = false) @RequestParam(value = "jurisdictionType",
+                    defaultValue = "", required = false) String jurisdictionType) {
 
         try {
 
-            ValidateApplicantForSharingOrdsResponse _response = figservice.validateApplicantForSharing(new ValidateApplicantForSharingRequest(applPartyId, jurisdictionType));
+            ValidateApplicantForSharingOrdsResponse _response =
+                    applicantService.validateApplicantForSharing(new ValidateApplicantForSharingRequest(applPartyId,
+                            jurisdictionType));
 
             return new ValidateApplicantForSharingResponse(_response.getValidationResult(),
                     Integer.parseInt(_response.getStatusCode()),
@@ -58,6 +62,6 @@ public class ValidateApplicantForSharingController {
                     FigaroValidationServiceConstants.VALIDATION_SERVICE_FAILURE_CD,
                     FigaroValidationServiceConstants.VALIDATION_SERVICE_BOOLEAN_FALSE);
         }
-    }
 
+    }
 }
