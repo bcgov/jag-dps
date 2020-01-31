e7fdcdbcd0a828834eba91b45d2ad1c15a7053d6
Nancy <nancy.zacharias@outlook.com>
2020-01-31 10:52:36 -0800

    Initial SPD(CRC) Worker app creation

---
+++ src/notificationworker/src/test/java/ca/bc/gov/open/pssg/rsbc/dps/spd/notificationworker/NotificationworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.open.pssg.rsbc.dps.spd.notificationworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
