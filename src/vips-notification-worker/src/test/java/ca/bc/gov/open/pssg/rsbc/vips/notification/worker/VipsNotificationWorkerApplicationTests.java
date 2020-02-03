f2a6a327a14c2ccd2de271d2a70cac49b81410ce
alex <alex.joyeux@gov.bc.ca>
2020-02-03 11:59:13 -0800

    Added new base app for vips worker

---
+++ src/vips-notification-worker/src/test/java/ca/bc/gov/open/pssg/rsbc/vips/notification/worker/VipsNotificationWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.open.pssg.rsbc.vips.notification.worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VipsNotificationWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
