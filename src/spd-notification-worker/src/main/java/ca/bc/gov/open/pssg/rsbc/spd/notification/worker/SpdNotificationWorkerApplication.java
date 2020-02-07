5cd1cccb333f644e9abcb00b8fe0dfe23101172a
alex <alex.joyeux@gov.bc.ca>
2020-02-07 14:36:42 -0800

    Changed crrp worker to spd

---
+++ src/spd-notification-worker/src/main/java/ca/bc/gov/open/pssg/rsbc/spd/notification/worker/SpdNotificationWorkerApplication.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.open.pssg.rsbc.spd.notification.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpdNotificationWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpdNotificationWorkerApplication.class, args);
+	}
+
+}
