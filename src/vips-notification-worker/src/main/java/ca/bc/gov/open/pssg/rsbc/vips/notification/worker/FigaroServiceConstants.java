a05f83b490b4bae93d12caa46a1b96e7a9370097
Carol Carpenter <Carol11.Carpenter@nttdata.com>
2020-02-11 07:51:30 -0800

    Add the VIPS Ords

---
+++ src/vips-notification-worker/src/main/java/ca/bc/gov/open/pssg/rsbc/vips/notification/worker/FigaroServiceConstants.java
@@ -0,0 +1,28 @@
+package ca.bc.gov.open.pssg.rsbc.vips.notification.worker;
+
+/**
+ * 
+ * Figaro Figaro service constants.
+ * 
+ * @author carolcarpenterjustice
+ *
+ */
+public class FigaroServiceConstants {
+
+	private FigaroServiceConstants() {}
+
+	// Response code types; anything >= 0 is good, anything < 0 is bad. 
+	public static final int FIGARO_SERVICE_FAILURE_CD = -1;
+	public static final int FIGARO_SERVICE_SUCCESS_CD = 0;
+	
+	// Response message types
+	public static final String FIGARO_SERVICE_RESP_MSG_FAIL = "fail";
+
+	// boolean type constants
+	public static final String FIGARO_SERVICE_BOOLEAN_FALSE = "F";
+	public static final String FIGARO_SERVICE_BOOLEAN_TRUE = "T";
+	
+	
+	
+}
+
