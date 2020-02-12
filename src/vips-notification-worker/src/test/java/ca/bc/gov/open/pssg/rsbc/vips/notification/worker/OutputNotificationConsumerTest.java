46aecede33cfef9bdea148fa3cf4a28b458d43c7
Carol Carpenter <Carol11.Carpenter@nttdata.com>
2020-02-12 07:40:52 -0800

    Fix unit test

--- src/vips-notification-worker/src/test/java/ca/bc/gov/open/pssg/rsbc/vips/notification/worker/OutputNotificationConsumerTest.java
+++ src/vips-notification-worker/src/test/java/ca/bc/gov/open/pssg/rsbc/vips/notification/worker/OutputNotificationConsumerTest.java
@@ -12,8 +12,13 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import ca.bc.gov.open.pssg.rsbc.dps.vips.notification.worker.generated.models.Data;
+
 import javax.xml.bind.JAXBContext;
 import java.io.ByteArrayInputStream;
+import java.io.Reader;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class OutputNotificationConsumerTest {
@@ -31,13 +36,39 @@ public class OutputNotificationConsumerTest {
     @Mock
     private JAXBContext jaxbContextMock;
 
+    @Mock
+    private Unmarshaller unmarshallerMock;
+
+    @Mock
+    private Data dataMock;
+
     private ByteArrayInputStream fakeContent() {
-        String content = "fake content";
+        String content = "<?xml version=\"1.0\" encoding=\"utf-16\"?>\n" +
+                "<Data>\n" +
+                "  <BatchData>\n" +
+                "    <BatchID>81</BatchID>\n" +
+                "    <FaxReceivedDate>2019-12-16T23:23:58Z</FaxReceivedDate>\n" +
+                "    <ImportDate>2019-12-16</ImportDate>\n" +
+                "    <ImportID>{2a37d377-f099-4d8f-af7c-9e74c4fcc58f}</ImportID>\n" +
+                "    <OriginatingNumber></OriginatingNumber>\n" +
+                "  </BatchData>\n" +
+                "  <DocumentData>\n" +
+                "    <externalFile>E:\\VIPS\\Release\\0000002D.PDF</externalFile>\n" +
+                "    <adpNumber></adpNumber>\n" +
+                "    <documentPages>1</documentPages>\n" +
+                "    <dType>REQDOCMOC</dType>\n" +
+                "    <irpNumber>212112312</irpNumber>\n" +
+                "    <policeFileNo></policeFileNo>\n" +
+                "    <ulNumber></ulNumber>\n" +
+                "    <validationUser>KYLEAR_A</validationUser>\n" +
+                "    <vinoiNumber></vinoiNumber>\n" +
+                "  </DocumentData>\n" +
+                "</Data>";
         return new ByteArrayInputStream(content.getBytes());
     }
 
     @BeforeAll
-    public void setUp() {
+    public void setUp() throws JAXBException {
 
         MockitoAnnotations.initMocks(this);
 
@@ -47,6 +78,8 @@ public class OutputNotificationConsumerTest {
         SftpProperties sftpProperties= new SftpProperties();
         sftpProperties.setRemoteLocation(REMOTE_LOCATION);
 
+        Mockito.when(jaxbContextMock.createUnmarshaller()).thenReturn(unmarshallerMock);
+        Mockito.when(unmarshallerMock.unmarshal(Mockito.any(Reader.class))).thenReturn(dataMock);
 
         sut = new OutputNotificationConsumer(sftpServiceMock, sftpProperties, documentServiceMock, jaxbContextMock);
     }
@@ -58,9 +91,6 @@ public class OutputNotificationConsumerTest {
             OutputNotificationMessage message = new OutputNotificationMessage(Keys.VIPS_VALUE, CASE_1);
             sut.receiveMessage(message);
         });
-
     }
-
-
 }
 
