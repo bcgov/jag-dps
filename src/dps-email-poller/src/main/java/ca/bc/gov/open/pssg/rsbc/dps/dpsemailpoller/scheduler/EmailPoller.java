9a7f0a9da5b2a39e9274a2447555511136498de4
Carol Carpenter <Carol11.Carpenter@nttdata.com>
2020-03-04 13:24:30 -0800

    Fix build

--- src/dps-email-poller/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/dpsemailpoller/scheduler/EmailPoller.java
+++ src/dps-email-poller/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/dpsemailpoller/scheduler/EmailPoller.java
@@ -2,13 +2,13 @@ package ca.bc.gov.open.pssg.rsbc.dps.dpsemailpoller.scheduler;
 
 import ca.bc.gov.open.pssg.rsbc.dps.dpsemailpoller.email.EmailService;
 import microsoft.exchange.webservices.data.core.service.item.Item;
-import microsoft.exchange.webservices.data.search.FindItemsResults;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
 
 @Component
 public class EmailPoller {
@@ -25,9 +25,9 @@ public class EmailPoller {
 
         logger.info("Poll for emails - {} ", new Date()); // now);
 
-        FindItemsResults<Item> findResults = emailService.getDpsInboxEmails();
+        List<Item> findResults = emailService.getDpsInboxEmails();
 
-        logger.info("          found - " + Integer.toString(findResults.getTotalCount()) + " Messages found in your Inbox");
+        logger.info("          found - " + Integer.toString(findResults.size()) + " Messages found in your Inbox");
 
     }
 
