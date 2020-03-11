244fe64e9e0327939ad3c45cced165e803100cce
Carol Carpenter <Carol11.Carpenter@nttdata.com>
2020-03-10 18:58:18 -0700

    remove unused constants

--- src/dps-email-poller/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/dpsemailpoller/messaging/Keys.java
+++ src/dps-email-poller/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/dpsemailpoller/messaging/Keys.java
@@ -1,7 +1,5 @@
 package ca.bc.gov.open.pssg.rsbc.dps.dpsemailpoller.messaging;
 
-import org.apache.commons.lang3.StringUtils;
-
 /**
  * Holds application keys.
  *
@@ -15,46 +13,8 @@ public class Keys {
     // **************** SOAP CONFIGURATION ****************
 
     /**
-     * The xml namespace
-     */
-    public static final String NAMESPACE_URI = "https://github.com/bcgov/jag-dps";
-
-    /**
      * DO NOT CHANGE - The default email message value.
      */
     public static final String EMAIL_MESSAGE_VALUE = "emailMessage";
-
-    /**
-     * DO NOT CHANGE - The xsd files that defines requests and responses.
-     */
-    public static final String EMAIL_MESSAGE_XSD = EMAIL_MESSAGE_VALUE + ".xsd";
-
-    /**
-     * The name of the SOAP port.
-     */
-    public static final String EMAIL_MESSAGE_PORT = StringUtils.capitalize(EMAIL_MESSAGE_VALUE) + "Port";
-
-    /**
-     * The name of the request xml object.
-     */
-    public static final String EMAIL_MESSAGE_REQUEST = EMAIL_MESSAGE_VALUE + "Request";
-
-
-    // **************** APPLICATION CONFIGURATION ****************
-
-    /**
-     * The email message success code.
-     */
-    public static final String EMAIL_MESSAGE_RESPONSE_SUCCESS_CODE = "0";
-
-    /**
-     * The email message error code.
-     */
-    public static final String EMAIL_MESSAGE_RESPONSE_ERROR_CODE = "-1";
-
-    /**
-     * The email message success message.
-     */
-    public static final String EMAIL_MESSAGE_RESPONSE_SUCCESS_MESSAGE = "success";
 
 }
