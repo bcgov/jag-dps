e19848713de977229eb8a4e1d7c2ddb8c8eff039
TayGov <Taylor.Clausen@gov.bc.ca>
2020-03-19 07:40:32 -0700

    Adding new project

---
+++ src/dps-email-worker/README.md
@@ -0,0 +1,15 @@
+# DPS OUTPUT NOTIFICATION SERVICE
+
+DPS Email Worker subscribes to queue and gets messages. 
+
+## RUN
+
+```bash
+mvn spring-boot:run
+```
+
+## TEST
+
+```bash
+mvn test
+```
---
+++ src/dps-email-worker/src/test/java/ca/bc/gov/pssg/rsbc/dps/dpsemailworker/DpsEmailWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.pssg.rsbc.dps.dpsemailworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpsEmailWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
