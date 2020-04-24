cc59227f9df99d7c198b63b6d7853d2804a9862a
alex <alex.joyeux@gov.bc.ca>
2020-04-24 13:19:45 -0700

    Removed unused scheduler

--- src/dps-email-worker/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/dpsemailworker/DpsEmailWorkerApplication.java
+++ src/dps-email-worker/src/main/java/ca/bc/gov/open/pssg/rsbc/dps/dpsemailworker/DpsEmailWorkerApplication.java
@@ -2,10 +2,8 @@ package ca.bc.gov.open.pssg.rsbc.dps.dpsemailworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class DpsEmailWorkerApplication {
 
 	public static void main(String[] args) {
