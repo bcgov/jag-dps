5e4ee9b1c568bcdac806f99deb84b1364141d3d0
alex <alex.joyeux@gov.bc.ca>
2020-04-28 08:47:48 -0700

    fix scope of variables

--- src/libs/dps-monitoring/src/main/java/ca/bc/gov/dps/monitoring/NotificationService.java
+++ src/libs/dps-monitoring/src/main/java/ca/bc/gov/dps/monitoring/NotificationService.java
@@ -2,15 +2,25 @@ package ca.bc.gov.dps.monitoring;
 
 import org.slf4j.*;
 
+/***
+ * A service to help capturing notification messages for system errors.
+ * Do not put any confidential information when you log system errors.
+ *
+ * @author alexjoybc@github.com
+ */
 public class NotificationService {
 
+    protected NotificationService(){}
 
+    private static final String TYPE_KEY = "type";
+    private static final String TO_KEY = "to";
+    private static final String NOTIFICATION = "notification";
 
-
-    public static final String TYPE_KEY = "type";
-    public static final String TO_KEY = "to";
-    public static final String NOTIFICATION = "";
-
+    /**
+     * Wraps a log message with Mapped Diagnostic Context
+     * @param to
+     * @param loggerAction
+     */
     public static void notify(String to, LoggerAction loggerAction) {
 
         Logger logger =  LoggerFactory.getLogger(NotificationService.class);
