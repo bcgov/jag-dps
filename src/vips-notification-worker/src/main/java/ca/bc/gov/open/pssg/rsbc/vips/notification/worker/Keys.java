2f6799c74fb5113280959ce66a3817cd6d0f2433
alex <alex.joyeux@gov.bc.ca>
2020-06-04 15:28:26 -0700

    Removed unused key

--- src/vips-notification-worker/src/main/java/ca/bc/gov/open/pssg/rsbc/vips/notification/worker/Keys.java
+++ src/vips-notification-worker/src/main/java/ca/bc/gov/open/pssg/rsbc/vips/notification/worker/Keys.java
@@ -16,12 +16,7 @@ public class Keys {
      * DO NOT CHANGE - The name of the CRRP queue
      */
     public static final String VIPS_QUEUE_NAME = VIPS_VALUE + ".notification.Q";
-
-    /**
-     * DO NOT CHANGE - The default output notification value.
-     */
-    public static final String OUTPUT_NOTIFICATION_VALUE = "outputNotification";
-
+    
 
     public static final String APP_NAME = "vips-notification-worker";
 
